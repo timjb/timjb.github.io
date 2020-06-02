d95c6c24160e4ffd15ec7bceeee3489fdddfe983
Tim J. Baumann <tim@timbaumann.info>
2020-06-02 22:32:25 +0200

    Add Nix derivations

---
+++ default.nix
@@ -0,0 +1,34 @@
+{ nixpkgs ? import <nixpkgs> { config = { allowUnfree = true; }; }, compiler ? "ghc865" }:
+
+let
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages = if compiler == "default" then
+    pkgs.haskellPackages
+  else
+    pkgs.haskell.packages.${compiler};
+
+  siteGen = nixpkgs.callPackage ./generator.nix {
+    nixpkgs = nixpkgs;
+    compiler = compiler;
+  };
+
+in nixpkgs.stdenv.mkDerivation {
+  name = "timbaumann";
+
+  ${if !nixpkgs.stdenv.isDarwin then "LOCALE_ARCHIVE" else null} = "${nixpkgs.glibcLocales}/lib/locale/locale-archive";
+  ${if !nixpkgs.stdenv.isDarwin then "LC_ALL" else null} = "en_US.UTF-8";
+
+  preConfigure = ''export LANG="en_US.UTF-8";'';
+
+  src = nixpkgs.lib.cleanSource ./.;
+
+  buildPhase = ''
+    ${siteGen}/bin/timbaumann rebuild
+  '';
+  installPhase = ''
+    mkdir -p $out
+    cp -r _site/* $out/
+  '';
+
+}
---
+++ generator.nix
@@ -0,0 +1,13 @@
+{ nixpkgs ? import <nixpkgs> { config = { allowUnfree = true; }; }, compiler ? "default" }:
+
+let
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages = if compiler == "default" then
+    pkgs.haskellPackages
+  else
+    pkgs.haskell.packages.${compiler};
+
+  generator = haskellPackages.callCabal2nix "timbaumann" (./.) { };
+
+in if pkgs.lib.inNixShell then generator.env else generator
